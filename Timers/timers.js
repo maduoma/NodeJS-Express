1cf06d23bac1fd34a5c479afedd02038072fde6f
maduoma <18477889+maduoma@users.noreply.github.com>
2021-01-20 14:11:30 +0100

    Timers

---
+++ Timers/timers.js
@@ -0,0 +1,28 @@
+// setTimeout // setInterval
+
+console.log('.......setTimeout function example..........');
+const func = () => {
+  console.log('Hello after 4 seconds.');
+};
+setTimeout(func, 4 * 1000);
+
+const rocks = (who) => {
+  console.log(who + ' rocks!');
+};
+setTimeout(rocks, 4 * 1000, 'Pluralsight');
+// Challenge 1
+const theOneFunc = (greet) => {
+  console.log('Hello after ' + greet);
+};
+setTimeout(theOneFunc, 4 * 1000, '4 seconds.');
+setTimeout(theOneFunc, 8 * 1000, '8 seconds.');
+
+// Same challenge 1 in another way
+const theOneFunc1 = (delay) => {
+  console.log('Hello after ' + delay + ' seconds');
+};
+setTimeout(theOneFunc1, 4 * 1000, 4);
+setTimeout(theOneFunc1, 8 * 1000, 8);
+
+console.log('.......setTimeInterval function example..........');
+setInterval(() => console.log('Hello after 3 seconds'), 3 * 1000);
