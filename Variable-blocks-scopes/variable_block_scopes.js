8ec7a170afbd579170d262a2b93ecc7859ff48e4
maduoma <18477889+maduoma@users.noreply.github.com>
2021-01-20 17:39:24 +0100

    Variable And Block Scopes

---
+++ Variable-blocks-scopes/variable_block_scopes.js
@@ -0,0 +1,39 @@
+{
+  // Block scope
+  {
+    //Nested Block Scope
+    // use let, const
+  }
+}
+
+if (true) {
+  // Block Scope
+}
+
+for (let i = 1; i <= 10; i++) {
+  // Block Scope
+}
+
+function sum(a, b) {
+  // Function Scope
+  var result = a + b;
+}
+
+sum(4, 3);
+
+// Scalar values
+const answers = 46;
+const greetings = 'Hello';
+console.log(answers);
+
+//Arrays and Objects
+const numbers = [2, 4, 6];
+const person = {
+  firstName: 'John',
+  lastName: 'Johnson',
+  firstName: 'Jones',
+};
+person.firstName = 'Emeka';
+console.log(person.firstName);
+
+//Note: use const more often and then let in the case of counter. Avoid using var in modern JavaScript.
